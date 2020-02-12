b3f0bc798a2299ecb9b56c80a5c082515f3277ea
Shayan Davarzani <info@shayand.com>
2020-02-12 11:29:11 +0330

    constant modification

--- src/Constants.php
+++ src/Constants.php
@@ -128,7 +128,7 @@ class Constants
             switch ($this->insuranceType){
                 case 'third-party-insurance':
                     $selectedItemCode = array_search($this->insuranceProduct,$this->thirdpartyItemCode);
-                    $final['item'] = ['itemCode' => $selectedItemCode,'itemName' => 'بیمه شخص ثالث'];
+                    $final['item'] = ['itemCode' => $selectedItemCode,'itemName' => 'شخص ثالث سواری'];
                     break;
                 case 'auto-body-insurance':
 //                    $selectedItemCode = array_search($this->insuranceProduct,$this->autobodyItemCode);
